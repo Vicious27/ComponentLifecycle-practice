d8a2c8a0ac704e14a965173f6b9db0790cbba719
Raul Garcia <punkvomit21@gmail.com>
2021-03-11 03:01:26 -0500

    created counter and app class components, practiced mounting updating and unmounting lifecycles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React from 'react';
+import CountDown from './CountDown';
 import './App.css';
-import Counter from './Counter';
 
-function App() {
-  return (
-    <>
-      <header className="App-header">
-        <h1 id="title">
-          Component Lifecycle
-        </h1>
-      </header>
-      <Counter />
-    </>
-  );
+class App extends React.Component {
+  state = {
+    displayTimer: false
+  };
+
+  toggleTimer = () => {
+    this.setState({
+      displayTimer: !this.state.displayTimer
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.displayTimer ? (<CountDown toggleTimer={this.toggleTimer} />
+        ) : (
+          <button onClick={this.toggleTimer}>Start Timer</button>
+        )}
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/CountDown.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+class CountDown extends React.Component {
+  state = {
+    count: 3
+  };
+
+  timerId;
+
+  componentDidMount() {
+    this.timeId = setInterval(() => {
+      this.setState({ count: this.state.count - 1 })
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    if (this.state.count === 0)
+      this.props.toggleTimer();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeId);
+    console.log("Timer about to unmount!");
+  }
+
+  render() {
+    return (
+      <p>Wait {this.state.count} seconds</p>
+    );
+  }
+};
+
+export default CountDown;
