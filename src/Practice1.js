a3c9f30f1ed5d4636a04725c71cbc4199532bebc
Raul Garcia <punkvomit21@gmail.com>
2021-03-12 17:59:49 -0500

    finished exercise 1 using getDerivedStateFromProps()

--- src/Practice1.js
+++ src/Practice1.js
@@ -6,18 +6,20 @@ class Header extends React.Component {
   state = {
     username: "defaultusername"
   };
-  /* 
-    1. Call static getDerivedStateFromProps(props, state) {}
-    2. Inside of that log out the props and state
-    3. Then create a newState object where you override the current username
-    4. Return the newState
-  */
+
+  static getDerivedStateFromProps(props, state) {
+    const newState = {
+      username: "Raul Ways"
+    }
+    return newState;
+  }
+
   render() {
     return (
-      <header>
+      < header >
         <h1>{this.props.sitename}</h1>
         <p>Hi {this.state.username}!</p>
-      </header>
+      </header >
     );
   }
 }
